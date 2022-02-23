290dbe85424c21c6d5d337430b93fb4730b0b223
Luqman <luqmanhaqeemsamsul@gmail.com>
2022-02-23 08:46:11 +0800

    add election controller and model

---
+++ app/Http/Controllers/ElectionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Electio;
+use Illuminate\Http\Request;
+
+class ElectionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Electio  $electio
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Electio $electio)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Electio  $electio
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Electio $electio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Electio  $electio
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Electio $electio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Electio  $electio
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Electio $electio)
+    {
+        //
+    }
+}
