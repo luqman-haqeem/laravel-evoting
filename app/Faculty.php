a5c0c4ba8f25746ea9addf83df8a28ad8d071be3
Luqman <luqmanhaqeemsamsul@gmail.com>
2022-03-11 11:04:53 +0800

    Add faculty model,migration and seeder

---
+++ app/Faculty.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Faculty extends Model
+{
+    use HasFactory,SoftDeletes;
+    public function voter()
+    {
+        return $this->hasMany(Voter::class);
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Faculty;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +16,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+    //     User::create([
+    //         'name' => 'Admin',
+    //         'last_name' => 'Admin',
+    //         'email' => 'admin@admin.com',
+    //         'email_verified_at' => now(),
+    //         'password' => Hash::make('password'), // password
+    //         'remember_token' => Str::random(10),
+    //    ]);
+    
+        Faculty::create([
+            'name' => 'FPM',
+            'fullname' => 'FAKULTI PENGURUSAN DAN MUAMALAH',
+        ]);
+        Faculty::create([
+            'name' => 'FSTM',
+        'fullname' => 'FAKULTI SAINS DAN TEKNOLOGI MAKLUMAT',
+        ]);
+        Faculty::create([
+            'name' => 'FPPI',
+            'fullname' => 'FAKULTI PENGAJIAN PERADABAN ISLAM',
+        ]);
+        Faculty::create([
+            'name' => 'FP',
+            'fullname' => 'FAKULTI PENDIDIKAN',
+        ]);
+        Faculty::create([
+            'name' => 'FSU',
+            'fullname' => 'FAKULTI SYARIAH DAN UNDANG-UNDANG',
+        ]);
+
         // $this->call(UsersTableSeeder::class);
+        // $this->call(FacultySeeder::class);
     }
 }
