adc00441620303a17b16a6fcbd4be3e2510b39b1
Luqman <luqmanhaqeemsamsul@gmail.com>
2022-08-28 19:12:08 +0800

    route update

--- routes/web.php
+++ routes/web.php
@@ -19,37 +19,39 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/profile', 'ProfileController@index')->name('profile');
-Route::put('/profile', 'ProfileController@update')->name('profile.update');
-
-// Route::post('/voters/import', 'VoterController@import')->name('voters.import');
-Route::resource('voters', 'VoterController');
-
-// Candidate
-Route::get('/elections/{election}/candidates/','CandidateController@index')->name('candidates.index');
-Route::post('/elections/{election}/candidates/','CandidateController@store')->name('candidates.store');
-Route::get('/elections/{election}/candidates/create','CandidateController@create')->name('candidates.create');
-Route::get('/elections/{election}/candidates/{candidate}','CandidateController@edit')->name('candidates.edit');
-Route::put('/elections/{election}/candidates/{candidate}','CandidateController@update')->name('candidates.update');
-Route::delete('/elections/{election}/candidates/{candidate}','CandidateController@destroy')->name('candidates.delete');
-
-// Voter
-Route::get('/elections/{election}/voters/','VoterController@index')->name('voters.index');
-Route::post('/elections/{election}/voters/','VoterController@store')->name('voters.store');
-Route::get('/elections/{election}/voters/create','VoterController@create')->name('voters.create');
-Route::get('/elections/{election}/voters/{voter}','VoterController@edit')->name('voters.edit');
-Route::put('/elections/{election}/voters/{voter}','VoterController@update')->name('voters.update');
-Route::delete('/elections/{election}/voters/{voter}','VoterController@destroy')->name('voters.destroy');
-Route::post('/elections/{election}/voters/import', 'VoterController@import')->name('voters.import');
+Route::middleware(['auth'])->group(function () {
 
 
-// Route::resource('candidates', 'CandidateController');
-Route::resource('elections', 'ElectionController');
+    // Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('/home', 'ElectionController@index')->name('home');
 
+    Route::get('/profile', 'ProfileController@index')->name('profile');
+    Route::put('/profile', 'ProfileController@update')->name('profile.update');
 
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
+    // Route::post('/voters/import', 'VoterController@import')->name('voters.import');
+    // Route::resource('voters', 'VoterController');
+
+    Route::resource('elections', 'ElectionController');
+
+    // Candidate
+    Route::get('/elections/{election}/candidates/', 'CandidateController@index')->name('candidates.index');
+    Route::post('/elections/{election}/candidates/', 'CandidateController@store')->name('candidates.store');
+    Route::get('/elections/{election}/candidates/create', 'CandidateController@create')->name('candidates.create');
+    Route::get('/elections/{election}/candidates/{candidate}', 'CandidateController@edit')->name('candidates.edit');
+    Route::put('/elections/{election}/candidates/{candidate}', 'CandidateController@update')->name('candidates.update');
+    Route::put('/elections/{election}/candidates-image/{candidate}', 'CandidateController@update_image')->name('candidates.update_image');
+    Route::delete('/elections/{election}/candidates/{candidate}', 'CandidateController@destroy')->name('candidates.delete');
+
+    // Voter
+    Route::get('/elections/{election}/voters/', 'VoterController@index')->name('voters.index');
+    Route::post('/elections/{election}/voters/', 'VoterController@store')->name('voters.store');
+    Route::get('/elections/{election}/voters/create', 'VoterController@create')->name('voters.create');
+    Route::get('/elections/{election}/voters/{voter}', 'VoterController@edit')->name('voters.edit');
+    Route::put('/elections/{election}/voters/{voter}', 'VoterController@update')->name('voters.update');
+    Route::delete('/elections/{election}/voters/{voter}', 'VoterController@destroy')->name('voters.destroy');
+    Route::post('/elections/{election}/voters/import', 'VoterController@import')->name('voters.import');
+
+    Route::get('/about', function () {
+        return view('about');
+    })->name('about');
+});
