d23bbea7f84dcceb321e43e00674929da6ce24a7
Luqman <luqmanhaqeemsamsul@gmail.com>
2022-08-31 21:57:27 +0800

    fix login issue

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -13,57 +13,37 @@ class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        // $validator = Validator::make($request->all(), [
-        //     'name' => 'required|string|max:255',
-        //     'email' => 'required|string|email|max:255|unique:users',
-        //     'password' => 'required|string|min:8'
-        // ]);
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8'
+        ]);
 
-        // if ($validator->fails()) {
-        //     return response()->json($validator->errors());
-        // }
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
 
-        // $user = User::create([
-        //     'name' => $request->name,
-        //     'last_name' => 'test',
-        //     'email' => $request->email,
-        //     'password' => Hash::make($request->password)
-        // ]);
+        $user = User::create([
+            'name' => $request->name,
+            'last_name' => 'test',
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
 
-        // $token = $user->createToken('auth_token')->plainTextToken;
-
-        $user = new User;
-     $user->name = 'joe';
-     $user->last_name = 'joe';
-     $user->email = 'joe@gmail.com';
-     $user->password = Hash::make('123456');
-
-     if ( ! ($user->save()))
-     {
-         dd('user is not being saved to database properly - this is the problem');          
-     }
-
-     if ( ! (Hash::check('123456', Hash::make('123456'))))
-     {
-         dd('hashing of password is not working correctly - this is the problem');          
-     }
-
-     if ( ! (Auth::attempt(array('email' => 'joe@gmail.com', 'password' => '123456'))))
-     {
-         dd('storage of user password is not working correctly - this is the problem');          
-     }
-
-     else
-     {
-         dd('everything is working when the correct data is supplied - so the problem is related to your forms and the data being passed to the function');
-     }
+        $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()
             ->json(['data' => $user, 'access_token' => $token, 'token_type' => 'Bearer',]);
     }
     public function login(Request $request)
     {
-        if (!Auth::attempt($request->only('email', 'password'))) {
+
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
             return response()
                 ->json(['message' => 'Unauthorized'], 401);
         }
